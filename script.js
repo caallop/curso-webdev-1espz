6fbf31f2bd8c5a02221a82db83f350fdd418fff0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 20:05:36 -0300

    alguns comando js e criaçao do arquivo anotaçao, importante para revisoes futuras

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Mensagem normal");
+console.log("Mensagem normal");
+console.log("Mensagem normal");
+console.log("Mensagem normal");
+
+//da informaçoes importandtes
+console.info("eu estou informando que vou surtar em 4 minutis")
+
+//avisa que vai dar ruim por n motivos
+console.warn("c pa vai dar ruim")
+
+//errou
+console.error("eita, deu ruim em")
+
+//array no console
+console.table([
+{
+    id:1, tarefa: "estudarJS"
+},
+{
+    id:2, tarefa: "estudarHTML" 
+}
+])
